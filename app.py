ba0599fe9b4a9b2e93431e42812e4e3e38d31edf
Mirthe Kuda <mirthe.kuda@outlook.com>
2023-09-27 11:11:23 +0200

    na 1 keer gaat de engelse franse woorden geven

--- app.py
+++ app.py
@@ -1,55 +1,86 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+from flask import session
 import random
 
+random.seed()
+
 app = Flask(__name__)
+app.secret_key ="abcdefghijklmnopqrstuvwxyz"
 
 woordenGenereren = True
 fr = []
 nl = []
-lijst = open('./data/Het eten.txt','r')
-for text in lijst:
+en = []
+Enl = []
+lijstFr = open('./data/Het eten.txt','r')
+for text in lijstFr:
     w = text.split(",")
     nl.append(w[0])
     fr.append(w[1].replace("\n", "")) 
+lijstEn = open('./data/The food.txt','r')
+for text in lijstEn:
+    w = text.split(",")
+    Enl.append(w[0])
+    en.append(w[1].replace("\n", "")) 
 tekstJ = 'correct.'
 tekstF = 'incorrect.'
 score = 0
-lengte = len(fr)
+lengte = 7
+oplossing =''
 
-#while woordenGenereren:
-optieA = random.randint(0,lengte)
-optieB = random.randint(0,lengte)  
-optieC = random.randint(0,lengte) 
-optieABC = random.randint(0,2)
-if optieA == optieB or optieB == optieC or optieC == optieA:
-    optieA = random.randint(0,lengte)
-    optieB = random.randint(0,lengte)  
-oplossingA = nl[optieA]
-oplossingB = nl[optieB]
-oplossingC = nl[optieC]
-if optieABC == 0:
-    fransWoord = fr[optieA]
-    oplossing = 'A'
-if optieABC == 1:
-    fransWoord = fr[optieB]
-    oplossing = 'B'
-if optieABC == 2:
-    fransWoord = fr[optieC]
-    oplossing = 'C'
 
+test = ""
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/Frans', methods = ['GET'])
-def frans(): 
-    return render_template('Frans.html', title='Quiz Frans', vraag = fransWoord , antwoord = oplossingA, antiwoord = oplossingB, woordant = oplossingC)
+def frans():
+    optieA = random.randint(0,lengte)
+    optieB = random.randint(0,lengte)  
+    optieC = random.randint(0,lengte) 
+    optieABC = random.randint(0,2)
+    if optieA == optieB or optieB == optieC or optieC == optieA:
+        optieA = random.randint(0,lengte)
+        optieB = random.randint(0,lengte)  
+    oplossingA = nl[optieA]
+    oplossingB = nl[optieB]
+    oplossingC = nl[optieC]
+    if optieABC == 0:
+        fransWoord = fr[optieA]
+        session["oplossing"] = 'A'
+    if optieABC == 1:
+        fransWoord = fr[optieB]
+        session["oplossing"] = 'B'
+    if optieABC == 2:
+        fransWoord = fr[optieC]
+        session["oplossing"] = 'C'
+    return render_template('Taal.html', title='Quiz Frans', vraag = fransWoord , antwoord = oplossingA, antiwoord = oplossingB, woordant = oplossingC)
 
 @app.route('/Engels', methods = ['GET'])
 def engels():
-    return render_template('Engels.html', title='Quiz Engels')
+    optieA = random.randint(0,lengte)
+    optieB = random.randint(0,lengte)  
+    optieC = random.randint(0,lengte) 
+    optieABC = random.randint(0,2)
+    if optieA == optieB or optieB == optieC or optieC == optieA:
+        optieA = random.randint(0,lengte)
+        optieB = random.randint(0,lengte)  
+    oplossingA = Enl[optieA]
+    oplossingB = Enl[optieB]
+    oplossingC = Enl[optieC]
+    if optieABC == 0:
+        engelsWoord = en[optieA]
+        session["oplossing"] = 'A'
+    if optieABC == 1:
+        engelsWoord = en[optieB]
+        session["oplossing"] = 'B'
+    if optieABC == 2:
+        engelsWoord = en[optieC]
+        session["oplossing"] = 'C'
+    return render_template('Taal.html', title='Quiz Engels', vraag = engelsWoord , antwoord = oplossingA, antiwoord = oplossingB, woordant = oplossingC)
 
 @app.route('/Duits', methods = ['GET'])
 def duits():
@@ -57,12 +88,17 @@ def duits():
 
 @app.route('/controleer',methods=['POST'])
 def controleer():
-    antwoord = request.form.get("antwoord")
+    antwoordA = request.form.get("antwoordA")
+    antwoordB = request.form.get("antwoordB")
+    antwoordC = request.form.get("antwoordC")
+    opl = session.get("oplossing")
+    print("oplossing: ")
+    print(opl)
     tekst = 'Wat denk je zelf?'
-    if antwoord == oplossing:
+    if antwoordA == session["oplossing"] or antwoordB == session["oplossing"] or antwoordC== session["oplossing"]:
         tekst = 'Uw antwoord is correct.'
     else:
-        tekst = 'Uw antwoord is foutief.'
+        tekst = 'Uw antwoord is fout.'
     return render_template('controleer.html', title = 'controle', tekst = tekst)
 
 app.run(host='0.0.0.0', port=9001)
